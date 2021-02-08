cab4025005aecabb832dea3085ed949d1b0152fd
Uwe L. Korn <uwe.korn@quantco.com>
2021-02-08 10:33:43 +0100

    Remove miniforge images as they move into their own repo

--- miniforge3/tests/tests/clean-homedir.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import os
-
-# .bash_logout could optionally be in $HOME, otherwise only .bashrc and .profile
-home_contents = sorted(os.listdir(os.environ['HOME']))
-if len(home_contents) == 3:
-    assert home_contents == ['.bash_logout', '.bashrc', '.profile'], home_contents
-else:
-    assert home_contents == ['.bashrc', '.profile'], home_contents
