a6dab12a70ee28e9b71e34113f09e19d7f7bc4a5
Marcel Bargull <marcel.bargull@udo.edu>
2023-11-22 09:05:28 +0100

    Use qemu-user-static 8.0.4 builds from Ubuntu
    The build for linux-anvil-aarch64 stalled with 8.1.2 from Debian.
    ref:
     https://github.com/conda-forge/docker-images/pull/246#issuecomment-1822444172
    
    Signed-off-by: Marcel Bargull <marcel.bargull@udo.edu>

--- download-qemu-static.sh
+++ download-qemu-static.sh
@@ -8,15 +8,15 @@ fi
 
 rm -f qemu-*-static
 
-version='8.1.2'
-build='ds-1'
+version='8.0.4'
+build='dfsg-1ubuntu5'
 curl -sL \
-    "http://ftp.debian.org/debian/pool/main/q/qemu/qemu-user-static_${version}+${build}_amd64.deb" |
+    "https://mirrors.edge.kernel.org/ubuntu/pool/universe/q/qemu/qemu-user-static_${version}%2B${build}_amd64.deb" |
     dpkg-deb --extract - ./deb-tmp
 mv ./deb-tmp/usr/bin/qemu-*-static ./
 rm -rf ./deb-tmp
 sha256sum --check << 'EOF'
-f5c0f9f9e1499c6907bb293a250d9a015ea99e304654e0ab9fb38fd72efad7ce  qemu-aarch64-static
-79a95114ab8b6f7d6d570bd6eeb81d8ed57f4d5210503a826a13fa26502a3bee  qemu-ppc64le-static
-e9c1ee2d9bf7e9aea9f59fd39837084f2c223676e656a5261f893b2de1b4e7bb  qemu-s390x-static
+0312d58f7f2e5825c84d9c39e686da8a2714728c5ed5b872716f06e189e6bbaa  qemu-aarch64-static
+6a9cc7d3d1a931811b1b91bbff83fb0bbfb0fb06cf8b7b980afad2ee55e3d06a  qemu-ppc64le-static
+8cf7e22ce2038cb36d9d0a9c49118e76a7fe6ab5d9c27d595b6815acc4b3bfac  qemu-s390x-static
 EOF
